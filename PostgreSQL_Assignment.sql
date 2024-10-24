2409637e7a928741044b86b2548115aaa8fca092
Rahad-Ullah <myemail.abc20@gmail.com>
2024-10-24 23:05:57 +0600

    Query-7 completed

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -105,3 +105,8 @@ SELECT c.course_name, COUNT(e.student_id) AS students_enrolled
 FROM courses c
 LEFT JOIN enrollment e ON c.course_id = e.course_id
 GROUP BY c.course_name
+
+
+-- Query 7: Calculate and display the average age of all students.
+SELECT AVG(age) AS average_age
+FROM students
