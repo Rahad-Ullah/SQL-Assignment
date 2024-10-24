d25a2aadf4a5ab761d6db8c62dd63ee8e7b2c903
Rahad-Ullah <myemail.abc20@gmail.com>
2024-10-24 21:58:04 +0600

    Data inserted to students, courses and enrollment table

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -8,14 +8,14 @@ CREATE TABLE students (
     frontend_mark INTEGER,
     backend_mark INTEGER,
     status VARCHAR(50)
-)
+);
 
 -- Create "courses" table
 CREATE TABLE courses (
     course_id SERIAL PRIMARY KEY,
     course_name VARCHAR(100) NOT NULL,
     credits INTEGER NOT NULL
-)
+);
 
 
 -- Create "enrollment" table
@@ -23,6 +23,37 @@ CREATE TABLE enrollment (
     enrollment_id SERIAL PRIMARY KEY,
     student_id INTEGER REFERENCES students (student_id) NOT NULL,
     course_id INTEGER REFERENCES courses (course_id) NOT NULL
-)
+);
 
+
+-- Data insertion to students table
+INSERT INTO students 
+    (student_name, age, email, frontend_mark, backend_mark, status)
+VALUES
+    ('Sameer', 21, 'sameer@example.com', 48, 60, NULL),
+    ('Zoya', 23, 'zoya@example.com', 52, 58, NULL),
+    ('Nabil', 22, 'nabil@example.com', 37, 46, NULL),
+    ('Rafi', 24, 'rafi@example.com', 41, 40, NULL),
+    ('Sophia', 22, 'sophia@example.com', 50, 52, NULL),
+    ('Hasan', 23, 'hasan@gmail.com', 43, 39, NULL);
+
+
+-- Data insertion to courses table
+INSERT INTO courses
+    (course_name, credits)
+VALUES
+    ('Next.js', 3),
+    ('React.js', 4),
+    ('Databases', 3),
+    ('Prisma', 3);
+
+
+-- Data insertion to enrollment table
+INSERT INTO enrollment
+    (student_id, course_id)
+VALUES
+    (1, 1),
+    (1, 2),
+    (2, 1),
+    (3, 2);
 
