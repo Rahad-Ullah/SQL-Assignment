e131d1e38f310a8e25b328742e4522c3541ee066
Rahad-Ullah <myemail.abc20@gmail.com>
2024-10-24 21:34:31 +0600

    Tables created students, courses and enrollment

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -1,1 +1,28 @@
 -- Active: 1729469441845@@127.0.0.1@5432@university_db@public
+-- Create "students" table 
+CREATE TABLE students (
+    student_id SERIAL PRIMARY KEY,
+    student_name VARCHAR(50) NOT NULL,
+    age INTEGER NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    frontend_mark INTEGER,
+    backend_mark INTEGER,
+    status VARCHAR(50)
+)
+
+-- Create "courses" table
+CREATE TABLE courses (
+    course_id SERIAL PRIMARY KEY,
+    course_name VARCHAR(100) NOT NULL,
+    credits INTEGER NOT NULL
+)
+
+
+-- Create "enrollment" table
+CREATE TABLE enrollment (
+    enrollment_id SERIAL PRIMARY KEY,
+    student_id INTEGER REFERENCES students (student_id) NOT NULL,
+    course_id INTEGER REFERENCES courses (course_id) NOT NULL
+)
+
+
